9f31884f0a431b35aef4a2da9ac654352b5094a4
ahmedmabdi <ahmed.abdi67@outlook.com>
2025-10-20 03:14:54 +0100

    add files

---
+++ eks.tf
@@ -0,0 +1,41 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name               = "eks-cluster"
+  kubernetes_version = "1.33"
+
+  addons = {
+    coredns = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      before_compute = true
+    }
+  }
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.public_subnets
+
+  endpoint_public_access                   = true
+  endpoint_public_access_cidrs             = ["0.0.0.0/0"]
+  enable_cluster_creator_admin_permissions = true
+
+  enable_irsa = false
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size   = 2
+      min_size       = 1
+      max_size       = 3
+      instance_types = ["t3.medium"]
+      capacity_type  = "ON_DEMAND"
+
+    }
+  }
+
+  tags = local.tags
+}
---
+++ locals.tf
@@ -0,0 +1,12 @@
+locals {
+  name        = "eks-cluster"
+  domain      = "ahmedumami.click"
+  region      = "eu-west-2"
+  hosted_zone = "arn:aws:route53:::hostedzone/Z103935430WUS287YMWJ6"
+
+
+  tags = {
+    project = "EKS PROJECT"
+    owner   = "Ahmed"
+  }
+}
