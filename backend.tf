e97ee98a1f0ecbec54ca069258497884e8b94138
ahmedmabdi <ahmed.abdi67@outlook.com>
2025-11-15 14:14:02 +0000

    section files

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "ny-terraform-states"
-    key = "project-eks/terraform.tfstate"
-    region = "eu-west-2"
-    dynamodb_table = "terraform-locks"
-    encrypt = true
-  }
-}
